1fb6ad0f825c40211c6f5323c41b96463bee1173
helix <garychang9@gmail.com>
2018-10-26 21:35:44 -0700

    Add new functions such as cancel, edit, and add new question

--- TrivialQ/app/src/main/java/com/example/helix/trivialq/AddCardActivity.java
+++ TrivialQ/app/src/main/java/com/example/helix/trivialq/AddCardActivity.java
@@ -4,18 +4,69 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddCardActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_card);
 
+        //Get previous question and answer choices
+        String prevQuestion = getIntent().getStringExtra("stringKey1");
+        String prevOption_1 = getIntent().getStringExtra("stringKey2");
+        String prevOption_2 = getIntent().getStringExtra("stringKey3");
+        String prevOption_3 = getIntent().getStringExtra("stringKey4");
+
+        //Get the editTexts to display previous data
+        EditText questionTV = ((EditText) findViewById(R.id.question));
+        EditText option_1TV = ((EditText) findViewById(R.id.answer_1));
+        EditText option_2TV = ((EditText) findViewById(R.id.answer_2));
+        EditText option_3TV = ((EditText) findViewById(R.id.answer_3));
+
+        //Displays previous data, if any
+        questionTV.setText(prevQuestion);
+        option_1TV.setText(prevOption_1);
+        option_2TV.setText(prevOption_2);
+        option_3TV.setText(prevOption_3);
+
+        //Cancel adding a question and answer
         findViewById(R.id.cancel_adding_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                finishActivity(50);
                 finish();
+            }
+        });
+
+        //Submits info and sends question and answer to main activity
+        findViewById(R.id.submit_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Retrieves input data
+                String question = ((EditText) findViewById(R.id.question)).getText().toString().trim();
+                String option_1 = ((EditText) findViewById(R.id.answer_1)).getText().toString().trim();
+                String option_2 = ((EditText) findViewById(R.id.answer_2)).getText().toString().trim();
+                String option_3 = ((EditText) findViewById(R.id.answer_3)).getText().toString().trim();
+
+                //Checks if all textfields were entered
+                if (question.isEmpty() || option_1.isEmpty() || option_2.isEmpty()|| option_3.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Please enter question and all answers", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //Packages data and send to MainActivity
+                    Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                    data.putExtra("question", question); // puts question string into the Intent, with the key as 'question'
+                    data.putExtra("option1", option_1);
+                    data.putExtra("option2", option_2);
+                    data.putExtra("option3", option_3);
+                    setResult(RESULT_OK, data); // set result code and bundle data for response
+                    finish();
+                }
             }
         });
     }
--- TrivialQ/app/src/main/java/com/example/helix/trivialq/MainActivity.java
+++ TrivialQ/app/src/main/java/com/example/helix/trivialq/MainActivity.java
@@ -1,41 +1,69 @@
 package com.example.helix.trivialq;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    boolean isShowingAnswers = true;
+//    boolean isShowingAnswers = true;
+    String correctString = "";
+    String questionString = "";
+    String optionString2 = "";
+    String optionString3 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /**
-         * Showing answer and hiding question
+//         * Showing answer and hiding question
+//
+//        findViewById(R.id.questionTV).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                findViewById(R.id.answerTV).setVisibility(View.VISIBLE);
+//                findViewById(R.id.questionTV).setVisibility(View.INVISIBLE);
+//            }
+//        });
+//
+//        /**
+//         * Showing question and hiding answer
+//
+//        findViewById(R.id.answerTV).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                findViewById(R.id.answerTV).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.questionTV).setVisibility(View.VISIBLE);
+//            }
+//        });
+//        findViewById(R.id.closed_eye_Img).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.closed_eye_Img).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.open_eye_Img).setVisibility(View.VISIBLE);
+//                isShowingAnswers = false;
+//            }
+//        });
+//
+//        findViewById(R.id.open_eye_Img).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                findViewById(R.id.closed_eye_Img).setVisibility(View.VISIBLE);
+//                findViewById(R.id.open_eye_Img).setVisibility(View.INVISIBLE);
+//                findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+//                findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+//                findViewById(R.id.answer3).setVisibility(View.VISIBLE);
+//                isShowingAnswers = true;
+//            }
+//        });
 
-        findViewById(R.id.questionTV).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.answerTV).setVisibility(View.VISIBLE);
-                findViewById(R.id.questionTV).setVisibility(View.INVISIBLE);
-            }
-        });
-
-        /**
-         * Showing question and hiding answer
-
-        findViewById(R.id.answerTV).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.answerTV).setVisibility(View.INVISIBLE);
-                findViewById(R.id.questionTV).setVisibility(View.VISIBLE);
-            }
-        });
-         */
         findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Resets all Textviews to gold color
         findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -68,36 +97,65 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.closed_eye_Img).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
-                findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
-                findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
-                findViewById(R.id.closed_eye_Img).setVisibility(View.INVISIBLE);
-                findViewById(R.id.open_eye_Img).setVisibility(View.VISIBLE);
-                isShowingAnswers = false;
-            }
-        });
-
-        findViewById(R.id.open_eye_Img).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.closed_eye_Img).setVisibility(View.VISIBLE);
-                findViewById(R.id.open_eye_Img).setVisibility(View.INVISIBLE);
-                findViewById(R.id.answer1).setVisibility(View.VISIBLE);
-                findViewById(R.id.answer2).setVisibility(View.VISIBLE);
-                findViewById(R.id.answer3).setVisibility(View.VISIBLE);
-                isShowingAnswers = true;
-            }
-        });
-
+        //Switch to AddCardActivity to add answer and question
         findViewById(R.id.adding_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivity(intent);
+                MainActivity.this.startActivityForResult(intent,100);
             }
         });
+
+        //Gets question and answer from AddCArdActivity and calls onActivityResult method
+        findViewById(R.id.edit_info).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                intent.putExtra("stringKey1", questionString);
+                intent.putExtra("stringKey2", correctString);
+                intent.putExtra("stringKey3", optionString2);
+                intent.putExtra("stringKey4", optionString3);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    //Gets question and answer from AddCardActivity and assigns the data to global variables
+    //Displays all data to what user inputted for editTect fields
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //Data is assigned to global variables and snackbar is shown if successful
+        if (requestCode == 100) {
+            //Assigns inputs to appropriate variables
+            try {
+                questionString = data.getExtras().getString("question");
+                correctString = data.getExtras().getString("option1");
+                optionString2 = data.getExtras().getString("option2");
+                optionString3 = data.getExtras().getString("option3");
+
+
+                TextView questionText = (TextView) findViewById(R.id.questionTV);
+                TextView option1Text = (TextView) findViewById(R.id.answer1);
+                TextView option2Text = (TextView) findViewById(R.id.answer2);
+                TextView option3Text = (TextView) findViewById(R.id.answer3);
+
+                //Displays the answer choices
+                questionText.setText(questionString);
+                option1Text.setText(optionString2);
+                option2Text.setText(correctString);
+                option3Text.setText(optionString3);
+
+                //Shows snackbar to indicate all data is updated
+                Snackbar.make(questionText, R.string.snackbar_text, Snackbar.LENGTH_SHORT).show();
+            }
+            /**
+             * When questionString = data.getExtras().getString("question") throws a NullPointerException
+             * it signifies that the cancel icon was clicked on.
+             *
+             * Do nothing if the NullPointerException was thrown
+             */
+            catch (java.lang.NullPointerException e)
+            {
+            }
+        }
     }
 }
